1404fac93a5d15b9f1fa541d3b8d59ea0a8125a6
Diana <>
2018-10-19 09:35:56 -0400

    WIP read target value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate toml;
+
+use std::env;
+use std::fs;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use toml::Value;
+
+fn get_target() -> PathBuf {
+    let cargo = Path::new("Cargo.toml");
+    // let cargo = Path::new(r#"C:\_Diana\Projects\diaos\Cargo.toml"#);
+    let toml = {
+        let mut s = String::new();
+        fs::File::open(cargo)
+            .unwrap()
+            .read_to_string(&mut s)
+            .unwrap();
+        s
+    };
+    let target = toml.parse::<Value>().unwrap();
+    let target = target["package"]["metadata"]["cargo-sysroot"]["target"]
+        .as_str()
+        .unwrap();
+    cargo.with_file_name(target) //.canonicalize().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // HACK
+    let _ = env::set_current_dir(r#"C:\_Diana\Projects\diaos"#);
+    let target = get_target();
+
+    println!("{:#?}", target);
 }
